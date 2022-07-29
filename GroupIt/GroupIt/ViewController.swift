ad1cf9278898a08f0bcc780234e69b8e0c51ef55
Kristian Myzeqari <kristian.myzeqari@mail.mcgill.ca>
2022-07-29 14:14:37 -0400

    Created new project for storyboard interface

---
+++ GroupIt/GroupIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupIt
+//
+//  Created by Kristian Myzeqari on 2022-07-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
