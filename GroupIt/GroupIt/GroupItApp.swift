d7c255799dc286e8787a43e5fe22b4a8a70abb2a
Kristian Myzeqari <kristian.myzeqari@mail.mcgill.ca>
2022-07-29 13:17:58 -0400

    Created GroupIt project

---
+++ GroupIt/GroupIt/GroupItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupItApp.swift
+//  GroupIt
+//
+//  Created by Kristian Myzeqari on 2022-07-29.
+//
+
+import SwiftUI
+
+@main
+struct GroupItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
