cbd0ff473a98fbc749d4a9849b9e11f078afeb8b
Kristian Myzeqari <kristian.myzeqari@mail.mcgill.ca>
2022-08-01 16:45:49 -0400

    ok

---
+++ GroupIt/GroupIt/NewEventViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewEventViewController.swift
+//  GroupIt
+//
+//  Created by Kristian Myzeqari on 2022-08-01.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
